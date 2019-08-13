52281e8bbd2afb06fba1747027a585cbe488d148
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-08-13 12:16:57 +0300

    build Header /  Navigation

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,25 @@ import Login from './Auth/Login'
 import SearchLinks from './Link/SearchLinks'
 import LinkList from './Link/LinkList'
 import LinkDetail from './Link/LinkDetail'
+import Header from './Header'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" render={() => <Redirect to="/new/1" />} />
-        <Route path="/create" component={CreateLink} />
-        <Route path="/login" component={Login} />
-        <Route path="/search" component={SearchLinks} />
-        <Route path="/top" component={LinkList} />
-        <Route path="/new/:page" component={LinkList} />
-        <Route path="/link/:linkId" component={LinkDetail} />
-      </Switch>
+      <div className="app-container">
+        <Header />
+        <div className="route-container">
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+            <Route path="/create" component={CreateLink} />
+            <Route path="/login" component={Login} />
+            <Route path="/search" component={SearchLinks} />
+            <Route path="/top" component={LinkList} />
+            <Route path="/new/:page" component={LinkList} />
+            <Route path="/link/:linkId" component={LinkDetail} />
+          </Switch>
+        </div>
+      </div>
     </BrowserRouter>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,37 @@
 import React from 'react'
+import { withRouter, NavLink } from 'react-router-dom'
 
-export default function Header() {
-  return <div>Header</div>
+function Header() {
+  return (
+    <div className="header">
+      <div className="flex">
+        <img src="/logo.png" alt="Hooks News Logo" className="logo" />
+        <NavLink to="/" className="header-title">
+          Hooks News
+        </NavLink>
+        <NavLink to="/new" className="header-link">
+          new
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/top" className="header-link">
+          top
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/search" className="header-link">
+          search
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/create" className="header-link">
+          submit
+        </NavLink>
+      </div>
+      <div className="flex">
+        <NavLink to="/login" className="header-link">
+          login
+        </NavLink>
+      </div>
+    </div>
+  )
 }
+
+export default withRouter(Header)
