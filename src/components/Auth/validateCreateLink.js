09057ed15e26772281c5be5f72ca85cad5cffc44
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-08-14 16:59:02 +0300

    Build Create Link Form with Validation

--- src/components/Auth/validateCreateLink.js
+++ src/components/Auth/validateCreateLink.js
@@ -1,1 +1,19 @@
-export default function validateCreateLink(values) {}
+export default function validateCreateLink({ description, url }) {
+  let errors = {}
+
+  // Description Errors
+  if (!description) {
+    errors.description = 'Description required'
+  } else if (description.length < 10) {
+    errors.description = 'Description must be at least 10 characters'
+  }
+
+  // Url Errors
+  if (!url) {
+    errors.url = 'URL required'
+  } else if (!/^(ftp|http|https):\/\/[^ "]+$/.test(url)) {
+    errors.url = 'URL must be valid'
+  }
+
+  return errors
+}
--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,5 +1,48 @@
 import React from 'react'
+import useFormValidation from '../Auth/useFormValidation'
+import validateCreateLink from '../Auth/validateCreateLink'
+
+const INITIAL_STATE = {
+  description: '',
+  url: ''
+}
 
 export default function CreateLink(props) {
-  return <div>CreateLink</div>
+  const { handleSubmit, handleChange, values, errors } = useFormValidation(
+    INITIAL_STATE,
+    validateCreateLink,
+    handleCreateLink
+  )
+
+  function handleCreateLink() {
+    console.log('Link created!')
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-column mt3">
+      <input
+        onChange={handleChange}
+        value={values.description}
+        name="description"
+        placeholder="A description for your link"
+        autoComplete="off"
+        type="text"
+        className={errors.description && 'error-input'}
+      />
+      {errors.description && <p className="error-text">{errors.description}</p>}
+      <input
+        onChange={handleChange}
+        value={values.url}
+        name="url"
+        placeholder="The URL for the link"
+        autoComplete="off"
+        type="text"
+        className={errors.url && 'error-input'}
+      />
+      {errors.url && <p className="error-text">{errors.url}</p>}
+      <button type="submit" className="button">
+        Submit
+      </button>
+    </form>
+  )
 }
