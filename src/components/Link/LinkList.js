8de1786c6584a6d8cf48a9c247ef7eeb3fcb4c02
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-08-16 02:54:04 +0300

    Fetching Links with .get(), .onSnapshot()

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,5 +1,23 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { FirebaseContext } from '../../firebase'
 
 export default function LinkList(props) {
+  const { firebase } = useContext(FirebaseContext)
+
+  useEffect(() => {
+    getLinks()
+  }, [])
+
+  function getLinks() {
+    firebase.db.collection('links').onSnapshot(handleSnapshot)
+  }
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map(doc => {
+      return { id: doc.id, ...doc.data() }
+    })
+
+    console.log({ links })
+  }
   return <div>LinkList</div>
 }
